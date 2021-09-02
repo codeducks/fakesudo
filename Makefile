7f67cb5b1c7d3f126ae382856336ffe9b22a8f88
faulty <faultydev@gmail.com>
2021-09-02 15:45:04 +0200

    init2

---
+++ Makefile
@@ -0,0 +1,25 @@
+# COMPILE
+cc    = g++
+flags = -g
+
+build_dir  = ./build
+fake_bin   = sudo
+manage_bin = fakesudo
+
+# SYS
+final_dir = /usr/local/bin
+
+define compile_prod
+	$(cc) sudo.cpp   -o $(build_dir)/$(fake_bin)   $(flags)
+	$(cc) manage.cpp -o $(build_dir)/$(manage_bin) $(flags)
+endef
+
+install:
+	$(compile_prod)
+
+	./install.sh
+	cp $(build_dir)/$(fake_bin)   $(final_dir)
+	cp $(build_dir)/$(manage_bin) $(final_dir)
+
+compile:
+	$(compile_prod)
---
+++ sudo.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <nlohmann/json.hpp>
+#include <boost/algorithm/string/join.hpp>
+
+using namespace std;
+using json = nlohmann::json;
+
+int main(int argc, char **argv) {
+
+    ifstream config_file("./config.json");
+    json config;
+    config_file >> config;
+
+    vector<string> command_vec(argv + 1, argv + argc);
+
+    printf("[ ");
+    for (int i = 1; i < argc; i++) {
+        cout << argv[i] << " ";
+    }
+
+    printf("]\n");
+
+    string full_command = boost::join(command_vec, " ");
+    system(full_command.c_str());
+
+}
